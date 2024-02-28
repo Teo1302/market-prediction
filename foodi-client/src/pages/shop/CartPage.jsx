c44b04a421b206ea0715e3c4c6d9df7345680487
Teo1302 <teodora.radu1302@yahoo.com>
2024-02-28 15:57:39 +0200

    restruct

--- foodi-client/src/pages/shop/CartPage.jsx
+++ foodi-client/src/pages/shop/CartPage.jsx
@@ -4,17 +4,21 @@ import {} from "react-icons/fa"
 import { FaTrash } from 'react-icons/fa6';
 import Swal from 'sweetalert2'
 import { AuthContext } from '../../contexts/AuthProvider';
+import {Link} from 'react-router-dom';
+
 
 const CartPage = () => {
-  const [cart,refetch]=useCart();
-  const{user}=useContext(AuthContext);
-  const [cartItems, setcartItems]=useState([]);
- 
+  const { user } = useContext(AuthContext);
+  const [cart, refetch] = useCart();
+  console.log(cart)
+  const [cartItems, setCartItems] = useState([]);
+
+  // console.log(cartItems)
+
   // Calculate the total price for each item in the cart
   const calculateTotalPrice = (item) => {
     return item.price * item.quantity;
   };
-  // Handle quantity increase
   const handleIncrease = async (item) => {
     try {
       const response = await fetch(`http://localhost:6001/carts/${item._id}`, {
@@ -80,6 +84,7 @@ const CartPage = () => {
     }
   };
 
+
   // Calculate the cart subtotal
   const cartSubtotal = cart.reduce((total, item) => {
     return total + calculateTotalPrice(item);
@@ -101,7 +106,7 @@ const CartPage = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        axios.delete(`http://localhost:5000/carts/${item._id}`).then(response => {
+        axios.delete(`http://localhost:6001/carts/${item._id}`).then(response => {
           if (response) {
             refetch();
              Swal.fire("Deleted!", "Your file has been deleted.", "success");
@@ -113,96 +118,119 @@ const CartPage = () => {
       }
     });
   };
-
   return (
-    <div className='section-container'>
-
-        {/* banner */}
-            <div className='section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#f3f4f6] to-100%'>
-      <div className='py-24 flex flex-col  justify-between items-center gap-8'>
-        {/* text */}
-        <div className='space-y-7 px-4'>
-          <h2 className='md:text-6xl text-4xl font-bold text-green-500 md:leading-snug leading-snug transition-all duration-300 hover:scale-105'>
-            Elemente adaugate in Cos
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
+    {/* banner */}
+    <div className=" bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+      <div className="py-28 flex flex-col items-center justify-center">
+        {/* content */}
+        <div className=" text-center px-4 space-y-7">
+          <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+            Items Added to The<span className="text-green"> Cart</span>
           </h2>
         </div>
       </div>
     </div>
 
-{/* table for the cart */}
-<div>
-<div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead className='bg-green text-white rounded-sm'>
-      <tr>
-        <th>#</th>
-        <th>Food</th>
-        <th>Item Name </th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
- {cart.map((item , index)=> (
- <tr key={index}>
- <td>{index+1}</td>
-    <td>
-      <div className="flex items-center gap-3">
-        <div className="avatar">
-          <div className="mask mask-squircle w-12 h-12">
-            <img src={item.image} 
-            alt="" />
-          </div>
-        </div>
-        </div>
-    </td>
-    <td className='font-medium'>
-      {item.name}
-    </td>
-    <td>
-      <button className='btn btn-xs' onClick={ ()=> handleDecrease(item)}>-</button>
-      <input type="number" value={item.quatity} className="w-10 mx-2 text-center overflow-hidden appearance-none" onChange={() => console.log(item.quatity)}/>
-      <button className='btn btn-xs' onClick={ ()=> handleIncrease(item)} >+</button>
-    </td>
-    <td>
-      ${calculateTotalPrice(item).toFixed(2)}
-    </td>
-    <th>
-    <button className="btn btn-ghost btn-xs text-red" onClick={() => handleDelete(item)}>
-  <FaTrash />
-</button>
+    {/* cart table */}
 
-    </th>
-  </tr>
- )
- )}
-    </tbody>
-  
-  </table>
-</div>
-</div>
-{/* customer details */}
-<div className='my-12 flex flex-col md:flex-row justify-between items-start'>
-  <div className='md:w-1/2 space-y-3'>
-    <h3 className='font-medium'>Customer Details</h3>
-    <p>Name:{user.displayName}</p>
-    <p>Email:{user.email}</p>
-    <p>User_id:{user.uid}</p>
+    {
+      (cart.length > 0) ? <div>
+      <div className="">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead className="bg-green text-white rounded-sm">
+              <tr>
+                <th>#</th>
+                <th>Food</th>
+                <th>Item Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td className="font-medium">{item.name}</td>
+                  <td>
+                    <button
+                      className="btn btn-xs"
+                      onClick={() => handleDecrease(item)}
+                    >
+                      -
+                    </button>
+                    <input
+                      type="number"
+                      value={item.quantity}
+                      onChange={() => console.log(item.quantity)}
+                      className="w-10 mx-2 text-center overflow-hidden appearance-none"
+                    />
+                    <button
+                      className="btn btn-xs"
+                      onClick={() => handleIncrease(item)}
+                    >
+                      +
+                    </button>
+                  </td>
+                  <td>${calculateTotalPrice(item).toFixed(2)}</td>
+                  <td>
+                    <button
+                      className="btn btn-sm border-none text-red bg-transparent"
+                      onClick={() => handleDelete(item)}
+                    >
+                      <FaTrash />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            {/* foot */}
+          </table>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col md:flex-row justify-between items-start my-12 gap-8">
+        <div className="md:w-1/2 space-y-3">
+          <h3 className="text-lg font-semibold">Customer Details</h3>
+          <p>Name: {user?.displayName || "None"}</p>
+          <p>Email: {user?.email}</p>
+          <p>
+            User_id: <span className="text-sm">{user?.uid}</span>
+          </p>
+        </div>
+        <div className="md:w-1/2 space-y-3">
+          <h3 className="text-lg font-semibold">Shopping Details</h3>
+          <p>Total Items: {cart.length}</p>
+          <p>
+            Total Price:{" "}
+            <span id="total-price">${orderTotal.toFixed(2)}</span>
+          </p>
+          <button className="btn btn-md bg-green text-white px-8 py-1">
+            Procceed to Checkout
+          </button>
+        </div>
+      </div>
+    </div> : <div className="text-center mt-20">
+      <p>Cart is empty. Please add products.</p>
+      <Link to="/menu"><button className="btn bg-green text-white mt-3">Back to Menu</button></Link>
     </div>
-    <div className='md:w-1/2 space-y-3'>
-    <h3 className='font-medium'>Shopping Details</h3>
-    <p>Total Items:{cart.length}</p>
-    <p>Total Price:{" "}
-              <span id="total-price">${orderTotal.toFixed(2)}</span>
-            </p>
-   <button className='btn bg-green text-white'> Procceed Checkout</button>
-    </div>
+    }
+    
   </div>
-</div>
-  )
- }
+);
+};
 
 export default CartPage
--- foodi-server/index.js
+++ foodi-server/index.js
@@ -1,122 +1,40 @@
 const express = require('express')
 const app = express()
-const cors =require('cors')
-const port = process.env.port || 6001;
+const port = process.env.PORT || 6001
+const mongoose = require('mongoose');
 require('dotenv').config()
+const cors = require('cors')
 console.log(process.env.DB_USER)
+
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
+//MONGO DB CONFIG USING MONGOOSE
 //teodoraradu36
-//Tgj4GLKr2bmfXJcI
+//fLlmGqiyynfJtssj
 
-//mongodb config
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-client.ackmzek.mongodb.net/demo-foodi-client?retryWrites=true&w=majority&appName=demo-foodi-client`)
+    .then(
+        console.log("MongoDB Connected Successfully!")
+    )
+    .catch((error) => console.log("Error connecting to MongoDB", error));
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@demo-foodi-cluster.mchkcr3.mongodb.net/?retryWrites=true&w=majority&appName=demo-foodi-cluster`;
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
-
-    //database and collevtions 
-    const menuCollections = client.db("demo-foodi-client").collection("meniu");
-    const cartCollections = client.db("demo-foodi-client").collection("cartItems");
-
-       // all menu items operations 
-
-   app.get('/menu', async (req, res) => {
-    const result = await menuCollections.find().toArray();
-    res.send(JSON.stringify(result));
-  })
-  // all cart operations
-
-   // posting cart to db
-  // posting cart to db
-app.post('/carts', async (req, res) => {
-  const cartItem = req.body;
-  const result = await cartCollections.insertOne(cartItem);
-  res.send(result.ops[0]); // Send the inserted document
-});
-
-// get carts using email
-app.get('/carts', async (req, res) => {
-  const email = req.query.email;
-  const filter = { email: email };
-  const result = await cartCollections.find(filter).toArray(); // <-- Add parentheses here
-  res.send(result);
-});
-
-//get specific carts
-
-app.get('/carts/:id', async (req,res)=>{
-  const id = req.params.id;
-  const filter  = { _id : new ObjectId(id)};
-  const result = await  cartCollections.findOne(filter);
-  res.send(result)
-})
-
-//delete items from cart
-
-app.delete('/carts/:id',async(req,res)=> {
-  const id = req.params.id;
-  const filter  = { _id : new ObjectId(id)};
-  const result = await cartCollections.deleteOne(filter);
-  res.send(result)
-})
-
-//update carts quantity
-
-app.put("/carts/:id, ", async(req,res)=> {
-  const id = req.params.id;
-  const {quantity}= req.body;
-  const filter  = { _id : new ObjectId(id)};
-  const options= {upsert:true};
-  const updateDoc = {
-    $set: {
-      quantity: parseInt(quantity, 10),
-      
-    },
-  };
-  const result= await cartCollections.updateOne(filter,updateDoc,options); 
-});
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
-}
-run().catch(console.dir);
-
+//   import routes here
+const menuRoutes = require('./api/routes/menuRoutes');
+const cartRoutes = require('./api/routes/cartRoutes');
+const userRoutes = require('./api/routes/userRoutes')
+app.use('/menu', menuRoutes)
+app.use('/carts', cartRoutes);
+//app.use('/users', userRoutes);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
